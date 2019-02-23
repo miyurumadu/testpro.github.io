31f0a73ecddd785cebce8506c9cfed8ca0d6c96e
miyurumadu <miyurumadusanka@yahoo.com>
2019-02-24 03:45:48 +0530

    Initial dist subtree commit

---
+++ build/precache-manifest.2a7309c3b7f93295f4b7c82dcc2ad9f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48f7ddb6e6c7ab774865",
+    "url": "/jitpro.github.io/static/js/runtime~main.48f7ddb6.js"
+  },
+  {
+    "revision": "88500c443e2c69ad35cd",
+    "url": "/jitpro.github.io/static/js/main.88500c44.chunk.js"
+  },
+  {
+    "revision": "fc042165d05bac87347c",
+    "url": "/jitpro.github.io/static/js/1.fc042165.chunk.js"
+  },
+  {
+    "revision": "88500c443e2c69ad35cd",
+    "url": "/jitpro.github.io/static/css/main.cb1a871c.chunk.css"
+  },
+  {
+    "revision": "d62b632e7f657c80ef91f07697cae879",
+    "url": "/jitpro.github.io/index.html"
+  }
+];
